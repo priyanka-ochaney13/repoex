5be6af3688564cf6ae723d9724d813dfd658d1a3
priyanka-ochaney13 <priyanka.ochaney@gmail.com>
2025-06-09 18:25:35 +0530

    added header and footer

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <a href="/" className="logo">RepoX</a>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/privaterepo">Private Repo</a>
+                <a href="https://github.com/priyanka-ochaney13/repoex">GitHub</a>
+            </nav>
+        </header>
+    );
+}
